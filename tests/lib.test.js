49bf8b99dddab675398fad32d597c603a4037511
Domingo Ortiz <ortizdomingo@hotmail.com>
2021-03-09 21:21:17 -0300

    test structure of function getPrimeNumbersDescendingOrder

---
+++ tests/lib.test.js
@@ -0,0 +1,14 @@
+
+describe('getPrimeNumbersDescendingOrder', () => {
+    it('should throw if limit is not a integer', () => {
+
+    });
+
+    it('should throw if limit is less or equal than 1', () => {
+        
+    });
+
+    it('should return array sort in descending order of prime numbers', () => {
+        
+    });
+});
