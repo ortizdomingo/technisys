c1073f4340c98be1137aeac3a82b505cd2d8d249
Domingo Ortiz <ortizdomingo@hotmail.com>
2021-03-09 21:42:52 -0300

    refactoring getPrimeNumbersDescendigOrder

--- lib.js
+++ lib.js
@@ -1,23 +1,25 @@
 function getPrimeNumbersDescendingOrder(limit) {
-    if (!Number.isInteger(limit)) throw new Error('limit is not a integer');
-
-    if (limit <= 1) throw new Error('limit can not be equal or less than 1');
+    validateLimit(limit);
 
     const primeNumbers = [];
-    for (let number = limit; number >= 2; number--) {
-        if (isPrime(number)) primeNumbers.push(number);
-    }
+    for (let number = limit; number >= 2; number--) 
+        if (isPrime(number)) primeNumbers.push(number);    
 
     return primeNumbers;
 }
 
 function isPrime(number) {
-    for(let factor = 2; factor < number; factor++) {
-        if (number % factor === 0) {
+    for(let factor = 2; factor < number; factor++) 
+        if (number % factor === 0) 
             return false;
-        }
-    }
+
     return true;
+}
+
+function validateLimit(limit) {
+    if (!Number.isInteger(limit)) throw new Error('limit is not a integer');
+
+    if (limit <= 1) throw new Error('limit can not be equal or less than 1');
 }
 
 exports.getPrimeNumbersDescendingOrder = getPrimeNumbersDescendingOrder;
